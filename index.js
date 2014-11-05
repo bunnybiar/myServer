3d728db7cbe925fc3c103762a368f659cb1c2a9a
bunnybiar <baembia.bgc@gmail.com>
2014-11-05 16:03:48 +0700

    edit index.js

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ var express = require('express')
 var app = express()
 
 app.use(express.static(__dirname + '/public'));
+app.get('/api/book',function(req,res){
+	var books = [
+		{title: 'Ragnarok', price: 200},
+		{title: 'Maple', price: 300},
+		{title: 'Yulgang', price: 400}
+	];
+	res.send(books);
+})
 
 var server = app.listen(3000, function () {
 
